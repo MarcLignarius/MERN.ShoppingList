0801ef7a0eafa9cbd0e068d77408a6b1c7196f0e
Marc Davies <marcdaviesriot@gmail.com>
2019-10-11 15:32:42 -0700

    add list items functionality

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -31,6 +31,16 @@ class ShoppingList extends Component {
         >
           Add Item
         </Button>
+
+        <ListGroup>
+          <TransitionGroup className="shopping-list">
+            {items.map(({ id, name }) => (
+              <CSSTransition key={id} timeout={500} classNames="fade">
+                <ListGroupItem>{name}</ListGroupItem>
+              </CSSTransition>
+            ))}
+          </TransitionGroup>
+        </ListGroup>
       </Container>
     );
   }
