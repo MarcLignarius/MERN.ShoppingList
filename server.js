2112c60c6ed570862e252222f9db4891f92bb4e8
Marc Davies <marcdaviesriot@gmail.com>
2019-10-11 13:06:07 -0700

    update content

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
 
@@ -8,8 +8,13 @@ const app = express();
 app.use(bodyParser.json());
 
 // DB Config
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
 // Connect to Mongo
 mongoose.connect(db)
-    .then(() => console.log('MongoDB connected...'))
+  .then(() => console.log("MongoDB connected..."))
+  .catch(err => console.log(err));
+
+  const port = process.env.PORT || 5000;
+
+  app.listen(port, () => console.log(`Server started on port ${port}`));
