7db9ff8413c0bcb7de75feacede1c5d6474807db
Marc Davies <marcdaviesriot@gmail.com>
2019-10-11 10:43:11 -0700

    update content

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-// Bodyparser Middleware
+// Body Parser Middleware
 app.use(bodyParser.json());
 
 // DB Config
 const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo
+mongoose.connect(db)
+    .then(() => console.log('MongoDB connected...'))
