26382ec9da8624b31073208c8e46ad8f474cf4e9
Marc Davies <marcdaviesriot@gmail.com>
2019-10-15 15:57:06 -0700

    add user login error handling

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -40,8 +40,9 @@ export default function(state = initialState, action) {
       }
     case AUTH_ERROR:
     case LOGIN_FAIL:
-    case REGISTER_FAIL:
     case LOGOUT_SUCCESS:
+    case REGISTER_FAIL:
+      localStorage.removeItem('token');
       return {
         ...state,
         token: null,
