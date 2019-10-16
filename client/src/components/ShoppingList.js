0208a3a4eccd435054b9576a99681153964e3dde
Marc Davies <marcdaviesriot@gmail.com>
2019-10-15 20:50:59 -0700

    make add/delete available to logged in users only

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -7,6 +7,12 @@ import PropTypes from 'prop-types';
  
 class ShoppingList extends Component {
 
+  static propTypes = {
+    getItems: PropTypes.func.isRequired,
+    item: PropTypes.object.isRequired,
+    isAuthenticated: PropTypes.bool
+  }
+
   componentDidMount() {
     this.props.getItems();
   }
@@ -24,15 +30,15 @@ class ShoppingList extends Component {
             {items.map(({ _id, name }) => (
               <CSSTransition key={_id} timeout={1000} classNames="fade">
                 <ListGroupItem>
-                <Button
-                  className="remove-btn"
-                  color="danger"
-                  size="sm"
-                  onClick={this.onDeleteClick.bind(this, _id)}
-                >
-                &times;
-                </Button>
-                {name}
+                  {this.props.isAuthenticated ? <Button
+                    className="remove-btn"
+                    color="danger"
+                    size="sm"
+                    onClick={this.onDeleteClick.bind(this, _id)}
+                  >
+                  &times;
+                  </Button> : null}
+                  {name}
                 </ListGroupItem>
               </CSSTransition>
             ))}
@@ -43,13 +49,10 @@ class ShoppingList extends Component {
   }
 }
 
-ShoppingList.propTypes = {
-  getItems: PropTypes.func.isRequired,
-  item: PropTypes.object.isRequired
-}
 
 const mapStateToProps = (state) => ({
-  item: state.item
+  item: state.item,
+  isAuthenticated: state.auth.isAuthenticated
 })
 
 export default connect(mapStateToProps, { getItems, deleteItem })(ShoppingList);
